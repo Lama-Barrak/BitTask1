7729dea98c369abb7f8b251e1fb57f986b97e1ff
Lama <lama_lab88@hotmail.com>
2020-06-01 18:32:17 +0300

    task1 first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+$(function(){
+  getBpi("USD");
+});
+function getBpi(code)
+{ $.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json",function(data)
+   {
+      // console.log(data.bpi[code].rate);
+      var desc=data.bpi[code].description;
+      var rate= data.bpi[code].rate;
+     
+      document.getElementById("desc").innerHTML=desc;
+      document.getElementById("code").innerHTML=code;
+      document.getElementById("rate").innerHTML=rate;
+
+
+
+   
+           
+         });}
+
