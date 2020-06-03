3b554e87972a4a926cb9a87ee676166edae3b3d3
Lama-Barrak <lama_lab88@hotmail.com>
2020-06-03 16:30:09 +0300

    task1 Second Update

--- js/main.js
+++ js/main.js
@@ -2,8 +2,32 @@
 $(function(){
   getBpi("USD");
 });
+//get  api info with $.ajax
 function getBpi(code)
-{ $.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json",function(data)
+{ $.ajax({
+   url:"https://api.coindesk.com/v1/bpi/currentprice.json",
+   dataType:"json",
+   cashe:false,
+   success:function(data,status)
+   {var desc=data.bpi[code].description;
+      var rate= data.bpi[code].rate;
+     
+      document.getElementById("desc").innerHTML=desc;
+      document.getElementById("code").innerHTML=code;
+      document.getElementById("rate").innerHTML=rate;},
+      error:function(xhr,textStatus,err){
+         $(".w1").show();
+         console.log(xhr);
+         console.log(textStatus);
+         console.log(err);
+        
+      }
+
+});
+
+};
+//get  api info with $.getjson
+   /*$.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json",function(data)
    {
       // console.log(data.bpi[code].rate);
       var desc=data.bpi[code].description;
@@ -17,5 +41,6 @@ function getBpi(code)
 
    
            
-         });}
+         });*/
+     // }
 
